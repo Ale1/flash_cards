a8b852b1b9e6aa1f2b35d909dce883d86dbb7d5f
apprentice@devbootcamp.com <guillaume.galuz@gmail.com>
2013-09-27 17:54:38 -0700

    Merged with FileParser method, added output methods

--- flashcards.rb
+++ flashcards.rb
@@ -18,6 +18,12 @@ class Deck
   def draw
     @cards.shift
   end
+
+  def to_s
+    deck_string = ""
+    @cards.each { |card| deck_string << "#{card}\n"}
+    deck_string
+  end
    
   # @deck = Deck.new( filename )
   # @deck.cards
@@ -32,18 +38,36 @@ class Card
     @definition = word_definition_hash[:definition]
   end
 
+  def to_s
+    "#{@word}: #{@definition}"
+  end
+
 end
 
-class FileHandler
-  # GIVES US A ARRAY OF HASHES, EACH HASH HAS 2 KEYS/VALUES
+class FileHandler 
+  def self.parse_to_card_array(file)
+    # Takes filename as input
+    # Returns Array of Hash objects
+    # { :definition => word definition ,
+    #   :term => word name }
+    card_array = []
+    file_array = File.readlines(file)
+    file_array.map! { |line| line.chomp }
+    file_array.each_slice(3) do |single_card|
+      card_array << Hash[:definition, single_card[0], :word, single_card[1]] 
+    end
+   card_array
+  end
 end
+
+# puts FileHandler.parse_to_card_array('flashcard_samples.txt')
+
 
 class GameController
   
   def self.run(name_of_file_txt)
-  word_definition_hasharray = FileHandler.parse_to_card_hash(name_of_file_txt)
-  my_deck = Deck.new
-  word_definition_hash.each 
+    my_deck = Deck.new(name_of_file_txt)
+    puts my_deck
 
   # my_deck Card.new(word_definition_hash)
   end
@@ -51,6 +75,6 @@ class GameController
 end
 
 name_of_file_txt = ARGV[0]
-Controller.run(name_of_file_txt)
+GameController.run(name_of_file_txt)
 
 
